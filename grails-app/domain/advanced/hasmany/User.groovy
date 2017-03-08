f0a4d822692aa31cfb44393cc2a7b1c4b1392e8a
Jason Heithoff <jheithof@fau.edu>
2017-03-07 21:15:19 -0500

    Recreating the error NULL not allowed for column "USER_LIKE_POSTS_ID"; SQL statement: insert into user_post (user_disliked_posts_id, post_id) values (?, ?) [23502-193]

--- grails-app/domain/advanced/hasmany/User.groovy
+++ grails-app/domain/advanced/hasmany/User.groovy
@@ -3,7 +3,11 @@ package advanced.hasmany
 class User {
 
     static constraints = {
+        likePosts minSize: 1
+        dislikedPosts minSize: 1
     }
 
     static hasMany = [likePosts: Post, dislikedPosts: Post]
+    List likePosts = new ArrayList()
+    List dislikedPosts = new ArrayList()
 }
